1707477473dd37f9365abd3d990936d46c1b6f7e
Florian Gaston <florian@busolinea.com>
2018-08-10 15:56:02 +0000

    Merged in develop (pull request #1)
    Init frontend boilerplate
    
    Approved-by: julioponce <julio@busolinea.com>

---
+++ webpack.dev.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const path = require('path')
+
+const {
+  HotModuleReplacementPlugin
+} = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+let plugins = [
+  new HtmlWebpackPlugin({
+    title: 'Frontend boilerplate'
+  }),
+  new HotModuleReplacementPlugin()
+]
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  watch: true,
+  entry: {
+    main: [
+      './client/index.js'
+    ]
+  },
+  output: {
+    path: path.resolve('./public'),
+    filename: '[name].js',
+    publicPath: 'http://localhost:9090/'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: {
+        test: path.resolve(__dirname, 'node_modules')
+      },
+      loader: 'babel-loader'
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve('./client'),
+      path.resolve('./')
+    ],
+    extensions: ['.js', '.css', '.scss']
+  },
+  plugins,
+  devServer: {
+    port: 9090,
+    hot: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    historyApiFallback: true
+  }
+}
